d91551a239d6c1bca24e132b675c26adfd6ef27a
vhCabral <victor5717oficial@gmail.com>
2025-05-25 23:55:22 -0300

    controle-de-fluxo-dio

---
+++ controle-de-fluxoo/src/Contador.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o primeiro número: ");
+        int primeiroNum = scanner.nextInt();
+        System.out.println("Digite o segundo número: ");
+        int segundoNum = scanner.nextInt();
+
+        try {
+            contar(primeiroNum, segundoNum);
+        } catch (Exception e) {
+            System.out.println("O segundo número precisa ser maior que o primeiro");
+        }
+        scanner.close();
+    }
+
+    static void contar(int primeiroNum, int segundoNum) throws ParametrosInvalidosException{
+        if(primeiroNum > segundoNum){
+            throw new ParametrosInvalidosException();
+        }
+    int contagem = segundoNum - primeiroNum;
+    for(int x = 1;x <= contagem; x++){
+        System.out.println("Printando o número: "+ x);
+    }
+
+    }
+}
